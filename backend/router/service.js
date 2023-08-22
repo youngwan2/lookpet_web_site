7b9a25e79e38411947971b23b754b931faa1ef14
naritaeng <tjdwns2040@naver.com>
2023-08-22 16:57:11 +0900

    병원,문화시설 각 지역,카테고리별로 데이터 가져옴

--- backend/router/service.js
+++ backend/router/service.js
@@ -8,16 +8,66 @@ connection()
 
 // 동물병원정보
 router.get('/service/hospital', (req, res) => {
-  hospitalModel.find({}, { _id: 0 }).then((result) => {
-    res.json(result)
-  })
+  const { region, page } = req.query
+
+  const showItems = 20 // 페이지당 보여줄 항목 수
+
+  hospitalModel
+    .find({ address: new RegExp(region) })
+    .countDocuments({})
+    .count()
+    .then((totalCount) => {
+      const totalPage = Math.ceil(totalCount / showItems) // 총 페이지 수 계산
+
+      hospitalModel
+        .find({ address: new RegExp(region) })
+        .skip((page - 1) * showItems)
+        .limit(showItems)
+        .then((result) => {
+          console.log(result)
+          console.log('총페이지:', totalPage)
+          console.log(region, page)
+          res.json({ result, totalCount, page, totalPage })
+        })
+        .catch((error) => {
+          console.log('데이터 조회 중 에러:', error)
+        })
+    })
+    .catch((error) => {
+      console.log('데이터 수 조회 중 에러:', error)
+    })
 })
 
 // 반려동물 동반가능 문화시설 정보
 router.get('/service/culture', (req, res) => {
-  cultureModel.find({}, { _id: 0 }).then((result) => {
-    res.json(result)
-  })
+  const { category, page } = req.query
+
+  const showItems = 20 // 페이지당 보여줄 항목 수
+
+  cultureModel
+    .find({ info: new RegExp(category) })
+    .countDocuments({})
+    .count()
+    .then((totalCount) => {
+      const totalPage = Math.ceil(totalCount / showItems) // 총 페이지 수 계산
+
+      cultureModel
+        .find({ info: new RegExp(category) })
+        .skip((page - 1) * showItems)
+        .limit(showItems)
+        .then((result) => {
+          console.log(result)
+          console.log('총페이지:', totalPage)
+          console.log(category, page)
+          res.json({ result, totalCount, page, totalPage })
+        })
+        .catch((error) => {
+          console.log('데이터 조회 중 에러:', error)
+        })
+    })
+    .catch((error) => {
+      console.log('데이터 수 조회 중 에러:', error)
+    })
 })
 
 module.exports = router
