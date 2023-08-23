cae3ac2225f0289c8c0a2b8c4bbd6741cabdc2be
KYW <qodna25@gmail.com>
2023-08-23 15:26:01 +0900

    게시판 추가중(에디터만 추가함)

--- frontend/src/views/community/update.vue
+++ frontend/src/views/community/update.vue
@@ -1,11 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <QuillEditor theme="snow" toolbar="full" />
+  </div>
 </template>
 <script>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import '@vueup/vue-quill/dist/vue-quill.bubble.css'
 export default {
+  components: {
+    QuillEditor
+  },
   data() {
     return {}
   }
 }
 </script>
-<style scoped></style>
+<style></style>
