b669a59fbd62d0e45e5102b179cbeed564998e4b
KYW <qodna25@gmail.com>
2023-09-04 11:46:03 +0900

    강아지 품종 필터 검색 기능 추가

--- frontend/src/views/dogBreed.vue
+++ frontend/src/views/dogBreed.vue
@@ -15,7 +15,12 @@
             <path
               d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
             /></svg></label>
-        <input type="text" id="dog_search" @input.enter="dogInfoPickFromDB" />
+        <input
+          type="text"
+          id="dog_search"
+          v-model="dogName"
+          @input.enter="dogInfoPickFromDB"
+        />
       </form>
       <ul class="dog_ul" ref="cards">
         <li
@@ -46,7 +51,8 @@ export default {
   data() {
     return {
       dogBreedInfo: [],
-      isLoading: true
+      isLoading: true,
+      dogName: ''
     }
   },
   async mounted() {
@@ -67,6 +73,15 @@ export default {
       console.log(this.$refs.cards)
     },
     dogInfoPickFromDB() {
+      console.log(this.dogName)
+      axios
+        .get(`http://localhost:3000/breed/search?dogname=${this.dogName}`)
+        .then((result) => {
+          this.dogBreedInfo = result.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
   }
 }
