2358fafbe495c5d85e77c1be4083bafac28042c4
KYW <qodna25@gmail.com>
2023-09-04 17:27:01 +0900

    model :: 선택한 종에 대한 추가 정보를 불러오는 로직 작성 -> vuex 설정 후 데이터 패치

---
+++ frontend/src/components/addBreedInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="addbreed_container" :class="addClass">
+    <h1>추가 품종 정보를 제공하는 뷰</h1>
+    <div>{{ breedInfo }}</div>
+  </section>
+</template>
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const breedInfo = ref('')
+    const addClass = ref('on').value
+    return {
+      addClass,
+      breedInfo
+    }
+  }
+}
+</script>
+<style scoped>
+.addbreed_container {
+  min-height: 100vh;
+  max-width: 900px;
+  width: 100%;
+}
+</style>
